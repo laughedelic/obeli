b7fef6ef56e62f955ba7a4f6dea292fc4b5d7813
Alexey Alekhin <laughedelic@gmail.com>
2015-07-10 16:07:36 +0200

    Minimal working definition of arrows with obeli

--- src/main/scala/Obeli.scala
+++ src/main/scala/Obeli.scala
@@ -1,5 +1,47 @@
-package ohnosequences.obeli
+package ohnosequences
 
-class Obeli {
+object obeli {
+
+  trait AnyArrow {
+
+    type In
+    type Out
+
+    type Obelus <: Out --> In
+    val  obelus: Obelus
+  }
+
+  object AnyArrow {
+
+    type from[I] = AnyArrow { type In = I }
+    type to[O]   = AnyArrow { type Out = O }
+  }
+
+  type -->[I, O] = AnyArrow { type In = I; type Out = O }
+  type ~~>[I, O] = AnyArrow { type In <: I; type Out <: O }
+
+
+  case class Compose[
+    A, B, C,
+    F <: A ~~> B,
+    G <: B ~~> C
+  ](f: F, g: G) extends AnyArrow {
+
+    type In = A
+    type Out = C
+
+    type Obelus = Compose[C, B, A, G#Obelus, F#Obelus]
+    val  obelus = Compose[C, B, A, G#Obelus, F#Obelus](g.obelus, f.obelus)
+  }
+
+  type >>[
+    F <: AnyArrow,
+    G <: AnyArrow.from[F#Out]
+  ] = Compose[F#In, F#Out, G#Out, F, G]
+
+  def >>[
+    F <: AnyArrow,
+    G <: AnyArrow.from[F#Out]
+  ](f: F, g: G): F >> G = Compose(f, g)
 
 }
