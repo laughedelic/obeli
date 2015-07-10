528892f709e631c75bcb2087c0bebc47491b9049
Alexey Alekhin <laughedelic@gmail.com>
2015-07-10 17:11:10 +0200

    Something more involved; multiplication; simple example in the test

--- src/main/scala/Obeli.scala
+++ src/main/scala/Obeli.scala
@@ -4,8 +4,8 @@ object obeli {
 
   trait AnyArrow {
 
-    type In
-    type Out
+    type In; val in: In
+    type Out; val out: Out
 
     type Obelus <: Out --> In
     val  obelus: Obelus
@@ -20,6 +20,16 @@ object obeli {
   type -->[I, O] = AnyArrow { type In = I; type Out = O }
   type ~~>[I, O] = AnyArrow { type In <: I; type Out <: O }
 
+  abstract class Arrow[I, O](val in: I, val out: O) extends AnyArrow {
+    type In = I
+    type Out = O
+  }
+
+  case class id[X](x: X) extends Arrow[X, X](x, x) {
+    type Obelus = this.type
+    lazy val obelus = this: Obelus
+  }
+
 
   case class Compose[
     A, B, C,
@@ -28,10 +38,13 @@ object obeli {
   ](f: F, g: G) extends AnyArrow {
 
     type In = A
-    type Out = C
+    lazy val in = f.in
 
-    type Obelus = Compose[C, B, A, G#Obelus, F#Obelus]
-    val  obelus = Compose[C, B, A, G#Obelus, F#Obelus](g.obelus, f.obelus)
+    type Out = C
+    lazy val out = g.out
+
+    type     Obelus = Compose[C, B, A, G#Obelus, F#Obelus]
+    lazy val obelus = Compose[C, B, A, G#Obelus, F#Obelus](g.obelus, f.obelus)
   }
 
   type >>[
@@ -43,5 +56,32 @@ object obeli {
     F <: AnyArrow,
     G <: AnyArrow.from[F#Out]
   ](f: F, g: G): F >> G = Compose(f, g)
+
+}
+
+object monoid {
+
+  import obeli._
+
+  case class ×[A, B](a: A, b: B)
+
+  case class Multiply[
+    FA, FB, F <: FA ~~> FB,
+    GA, GB, G <: GA ~~> GB
+  ](f: F, g: G) extends AnyArrow {
+
+    type In = FA × GA
+    lazy val in = ×(f.in, g.in): In
+
+    type Out = FB × GB
+    lazy val out = ×(f.out, g.out): Out
+
+    type     Obelus = Multiply[FB, FA, F#Obelus, GB, GA, G#Obelus]
+    lazy val obelus = Multiply[FB, FA, F#Obelus, GB, GA, G#Obelus](f.obelus, g.obelus)
+  }
+
+  type ⊗[F <: AnyArrow, G <: AnyArrow] = Multiply[F#In, F#Out, F, G#In, G#Out, G]
+
+  def ⊗[F <: AnyArrow, G <: AnyArrow](f: F, g: G): F ⊗ G = Multiply(f, g)
 
 }
--- src/test/scala/Obeli.scala
+++ src/test/scala/Obeli.scala
@@ -1,16 +1,26 @@
-package ohnosequences.obeli.test
+package ohnosequences.test
 
 import org.scalatest.FunSuite
 
 import ohnosequences.obeli._
+import ohnosequences.monoid._
 
 class ObeliTest extends FunSuite {
 
+  case class right[X, Y](x: X, y: Y) extends Arrow[X, Y](x, y) {
+    type Obelus = left[Y, X]
+    lazy val obelus = left(y, x)
+  }
+
+  case class left[Y, X](y: Y, x: X) extends Arrow[Y, X](y, x) {
+    type Obelus = right[X, Y]
+    lazy val obelus = right(x, y)
+  }
+
+
   test("Dummy test coming from the template") {
 
-    assert(
+    >>(right(1, 'a'), left('b', 2)).obelus
 
-      12 === 12
-    )
   }
 }
